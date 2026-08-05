3757ea91b03bc786463c0e30aa1417b21adc34a6
LaoraSad <264025963+LaoraSad@users.noreply.github.com>
2026-08-05 00:51:23 -0500

    feat: register the billboard form under the home route

--- src/appRouter.tsx
+++ src/appRouter.tsx
@@ -1,9 +1,10 @@
 import { createBrowserRouter } from "react-router";
+import { Formulario } from "./features/billboard/pages";
 
 export const appRouter = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello World</div>,
+    element: <Formulario />,
   },
   {
     path: "/auth",
